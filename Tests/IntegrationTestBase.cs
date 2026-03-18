e3dd842f8253deaab80f91b958d6849660d3c9ca
Dominik Borawski <borawskid@icloud.com>
2026-03-18 21:25:18 +0100

    Testing async databases

--- Tests/IntegrationTestBase.cs
+++ Tests/IntegrationTestBase.cs
@@ -2,8 +2,6 @@ using BlogCore.DAL.Data;
 using BlogCore.DAL.Repositories;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Respawn;
-using Respawn.Graph;
 using Testcontainers.MsSql;
 
 namespace Tests;
@@ -15,8 +13,7 @@ public abstract class IntegrationTestBase
         .WithPassword("StrongPassword123!")
         .Build();
 
-    private static Respawner _respawner = null!;
-    private static string _connectionString = null!;
+    private string _connectionString = null!;
 
     protected BlogContext _context = null!;
     protected BlogRepository _repository = null!;
@@ -25,10 +22,16 @@ public abstract class IntegrationTestBase
     public static async Task AssemblyInit(TestContext context)
     {
         await _dbContainer.StartAsync();
+    }
+
+    [TestInitialize]
+    public async Task Setup()
+    {
+        var dbName = $"BlogCoreTests_{Guid.NewGuid():N}";
 
         var builder = new SqlConnectionStringBuilder(_dbContainer.GetConnectionString())
         {
-            InitialCatalog = "BlogCoreTests"
+            InitialCatalog = dbName
         };
 
         _connectionString = builder.ConnectionString;
@@ -37,52 +40,20 @@ public abstract class IntegrationTestBase
             .UseSqlServer(_connectionString)
             .Options;
 
-        using (var dbContext = new BlogContext(options))
-        {
-            await dbContext.Database.EnsureDeletedAsync();
-            await dbContext.Database.EnsureCreatedAsync();
-        }
-
-        using (var connection = new SqlConnection(_connectionString))
-        {
-            await connection.OpenAsync();
-
-            _respawner = await Respawner.CreateAsync(connection, new RespawnerOptions
-            {
-                TablesToIgnore = new Table[]
-                {
-                    new Table("__EFMigrationsHistory")
-                }
-            });
-        }
-    }
-
-    [TestInitialize]
-    public async Task Setup()
-    {
-        var options = new DbContextOptionsBuilder<BlogContext>()
-            .UseSqlServer(_connectionString)
-            .Options;
-
         _context = new BlogContext(options);
+        await _context.Database.EnsureCreatedAsync();
+
         _repository = new BlogRepository(_context);
-
-        await ResetDatabaseAsync();
-    }
-
-    protected async Task ResetDatabaseAsync()
-    {
-        using (var connection = new SqlConnection(_connectionString))
-        {
-            await connection.OpenAsync();
-            await _respawner.ResetAsync(connection);
-        }
     }
 
     [TestCleanup]
-    public void Cleanup()
+    public async Task Cleanup()
     {
-        _context.Dispose();
+        if (_context != null)
+        {
+            await _context.Database.EnsureDeletedAsync();
+            await _context.DisposeAsync();
+        }
     }
 
     [AssemblyCleanup]
